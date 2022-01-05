d72921d25f0b74695ad2c35dede9e5d2595e1bbc
Benedikt Jerat <b.jerat@googlemail.com>
2022-01-05 12:14:46 +0100

    Add debug instrumentation via ReactorDebugAgent

--- nonblocking/src/main/kotlin/de/dxfrontiers/nonblocking/Application.kt
+++ nonblocking/src/main/kotlin/de/dxfrontiers/nonblocking/Application.kt
@@ -2,10 +2,12 @@ package de.dxfrontiers.nonblocking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.tools.agent.ReactorDebugAgent
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
+    ReactorDebugAgent.init()
     runApplication<Application>(*args)
 }
