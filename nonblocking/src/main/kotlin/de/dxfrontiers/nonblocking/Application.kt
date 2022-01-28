6adccf50161721ff0af03e86fe5d3d2e0b4de24d
Benedikt Jerat <b.jerat@googlemail.com>
2022-01-28 12:08:16 +0100

    Add debug mode via Hooks.onOperatorDebug

--- nonblocking/src/main/kotlin/de/dxfrontiers/nonblocking/Application.kt
+++ nonblocking/src/main/kotlin/de/dxfrontiers/nonblocking/Application.kt
@@ -2,10 +2,12 @@ package de.dxfrontiers.nonblocking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.core.publisher.Hooks
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
+    Hooks.onOperatorDebug()
     runApplication<Application>(*args)
 }
